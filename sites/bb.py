1b69dddf77469d7cb419ec79b1077b10f52d8629
Blake Harris <blakebharris@gmail.com>
2016-10-02 10:57:47 -0700

    found product name, need to backtrack and find price

--- sites/bb.py
+++ sites/bb.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://www.bodybuilding.com/store/whey.html'
+url = 'http://www.bodybuilding.com/store/protein-powder.html'
 r = requests.get(url)
 
 soup = BeautifulSoup(r.content, "html.parser")
@@ -9,9 +9,15 @@ soup = BeautifulSoup(r.content, "html.parser")
 links = soup.find_all('a')
 
 gen = soup.find_all('article', {'class': 'product-layout'})
+print(gen)
 
-num = 1
 for item in gen:
-    print(num)
-    print(item.find_all('div', {'class': 'product-details'})[0].text)
-    num += 1
+    # Gets the product name
+    # print(item.find_all('div', {'class': 'product-details'})[0].find_all('a')[0].text)
+
+    # TODO Get Product Link
+
+    # TODO Get Product Price
+    # print(item)
+    break
+
