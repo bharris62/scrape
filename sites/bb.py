f6a976e3eccde9376eae42033d53ebc07549f8e2
Blake Harris <blakebharris@gmail.com>
2016-10-02 11:20:20 -0700

    needed to change from html-parser to lxml

--- sites/bb.py
+++ sites/bb.py
@@ -4,20 +4,18 @@ from bs4 import BeautifulSoup
 url = 'http://www.bodybuilding.com/store/protein-powder.html'
 r = requests.get(url)
 
-soup = BeautifulSoup(r.content, "html.parser")
+soup = BeautifulSoup(r.content, "lxml")
 
 links = soup.find_all('a')
 
 gen = soup.find_all('article', {'class': 'product-layout'})
-print(gen)
 
 for item in gen:
     # Gets the product name
-    # print(item.find_all('div', {'class': 'product-details'})[0].find_all('a')[0].text)
+    print(item.find_all('div', {'class': 'product-details'})[0].find_all('a')[0].text)
 
     # TODO Get Product Link
 
-    # TODO Get Product Price
-    # print(item)
+    # Get Product Price
+    print(item.find_all('li', {'itemprop': 'price'})[0].text)
     break
-
