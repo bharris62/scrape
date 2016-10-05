5bc4a09f8196ddf2decb0e5a16604669c8b271ef
Andrew Harris <blakebharris@gmail.com>
2016-10-04 21:46:38 -0500

    todos for supp warehouse

---
+++ supp_warehouse_whey.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+from database import session, Product
+
+
+url = ['http://www.bodybuilding.com/store/mic.html',
+       'http://www.bodybuilding.com/store/mic.html?pg=2',
+       'http://www.bodybuilding.com/store/mic.html?pg=3']
+
+rows_logged = 0
+for page in url:
+
+    r = requests.get(page)
+    soup = BeautifulSoup(r.content, "lxml")
+
+    gen = soup.find_all('article', {'class': 'product-layout'})
+    for item in gen:
+        product = Product()
+        # Dealer Name
+        website = 'Supplement Warehouse'
+        # product.product_dealer = website
+
+        # TODO Get the product name
+
+        # TODO Get Product Link
+
+        # TODO Get Product Price
+
+        # TODO Per Serving
+
+        # TODO Price per serving
+
+        # type of protein
+        prod_type = "Whey"
+        # product.product_type = prod_type
+
+        # TODO Product Image
+
+        session.add(product)
+        session.commit()
+
+
+print("logged {} rows".format(rows_logged))
