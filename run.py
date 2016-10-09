a825b1c9462d81d737571320cb690c13c8a6a65a
Blake Harris <blakebharris@gmail.com>
2016-10-09 16:12:20 -0700

    posting data

--- run.py
+++ run.py
@@ -8,19 +8,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    product_name = session.query(Product.product_name).first()
-    product_image = session.query(Product.product_image).first()
-    price_per_serving = session.query(Product.product_price_per_serving).first()
-    product_price = session.query(Product.product_price).first()
-    product_type = session.query(Product.product_type).first()
-    product_description = session.query(Product.product_description).first()
+    products = session.query(Product).all()
     return render_template('base.html',
-                           product_name=product_name[0],
-                           product_image=product_image[0],
-                           price_per_serving=price_per_serving[0],
-                           product_price=product_price[0],
-                           product_type=product_type[0],
-                           product_description=product_description[0])
+                           products=products)
 
 
 if __name__ == '__main__':
