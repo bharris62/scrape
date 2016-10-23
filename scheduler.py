9413286fd7d7db84e19b5ad3bd9046d02863b4a4
Andrew Harris <blakebharris@gmail.com>
2016-10-23 09:25:36 -0500

    slowed scraper to every hour

--- scheduler.py
+++ scheduler.py
@@ -4,7 +4,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=5)
+@sched.scheduled_job('interval', minutes=60)
 def timed_job():
     print('cron started')
     run_scrapers()
