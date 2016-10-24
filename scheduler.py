983561645325d735133be730b50cba27383e409a
Andrew Harris <blakebharris@gmail.com>
2016-10-23 22:02:53 -0500

    trying different scheduler

--- scheduler.py
+++ scheduler.py
@@ -4,8 +4,99 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=55)
-def timed_job():
+# @sched.scheduled_job('interval', minutes=55)
+# def timed_job():
+#     print('cron started')
+#     run_scrapers()
+#     print('cron finished')
+
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=0)
+def scheduled_job():
+    print('cron started')
+    run_scrapers()
+    print('cron finished')
+
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=2)
+def scheduled_job():
+    print('cron started')
+    run_scrapers()
+    print('cron finished')
+
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=4)
+def scheduled_job():
+    print('cron started')
+    run_scrapers()
+    print('cron finished')
+
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=6)
+def scheduled_job():
+    print('cron started')
+    run_scrapers()
+    print('cron finished')
+
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=8)
+def scheduled_job():
+    print('cron started')
+    run_scrapers()
+    print('cron finished')
+
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=10)
+def scheduled_job():
+    print('cron started')
+    run_scrapers()
+    print('cron finished')
+
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=12)
+def scheduled_job():
+    print('cron started')
+    run_scrapers()
+    print('cron finished')
+
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=14)
+def scheduled_job():
+    print('cron started')
+    run_scrapers()
+    print('cron finished')
+
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=16)
+def scheduled_job():
+    print('cron started')
+    run_scrapers()
+    print('cron finished')
+
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=18)
+def scheduled_job():
+    print('cron started')
+    run_scrapers()
+    print('cron finished')
+
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=20)
+def scheduled_job():
+    print('cron started')
+    run_scrapers()
+    print('cron finished')
+
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=22)
+def scheduled_job():
+    print('cron started')
+    run_scrapers()
+    print('cron finished')
+
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=24)
+def scheduled_job():
     print('cron started')
     run_scrapers()
     print('cron finished')
